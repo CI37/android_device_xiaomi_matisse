333ab5b766b19391f164bb40d2910b6dfb585be9
CI37 <2471149874@qq.com>
2025-02-02 03:05:34 +0800

    Initial commit
    Change-Id: I2d91530dd400f01b7023993ed2b406660fb66fc1

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_matisse.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_matisse-user \
+    lineage_matisse-userdebug \
+    lineage_matisse-eng
